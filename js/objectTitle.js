6afbf5a34f3e4e379d83faece22ae3dde81be360
Арсений <156520223+K0mentat1r@users.noreply.github.com>
2024-07-18 17:26:16 +0200

    Добавлены подсказки, ещё немного изменений
    Теперь есть подсказки, которые появляются при наведении на элемент, для которого эта подсказка существует. И ещё что то сделал, не помню что

---
+++ js/objectTitle.js
@@ -0,0 +1,24 @@
+let posX; //позиция мыши по x
+let posY; //позиция мыши по y
+
+document.addEventListener("mousemove", function(mousePos){ //получаем позицию мыши
+    posX = mousePos.clientX;
+    posY = mousePos.clientY;
+});
+
+//запускается при каждом движении мыши
+function followMouse(objectName, helpName){ //с элемента, у которого должна быть подсказка, получаем инфу об id элемента и div'а, который будет подсказкой
+    let elem = document.getElementById(objectName);
+    let elemHelp = document.getElementById(helpName);
+    if (typeof elem.onmousemove !== "function"){  //если на элементе ещё нет евент листенера, то добавляем
+        elem.onmousemove = function(){ //если курсор находится на элементе
+            elemHelp.style.display = "flex";
+            elemHelp.style.position = 'fixed';
+            elemHelp.style.left = posX + 'px';
+            elemHelp.style.top = posY + 'px';
+        }
+        elem.onmouseleave = function(){ //если курсор находится вне элемента
+            elemHelp.style.display = "none";
+        }
+    }
+}
